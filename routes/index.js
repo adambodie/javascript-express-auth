31044c5f894bb801a7ae8e79da7a1f5590fb2dad
Adam Bodie <adam.bodie@yahoo.com>
2018-08-19 16:54:58 -0700

    Added register route, SCSS compatibility

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,17 @@ router.get('/', function(req, res, next) {
   return res.render('index', { title: 'Home' });
 });
 
+// GET /register
+router.get('/register', function(req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+// GET /register
+router.post('/register', function(req, res, next) {
+  return res.render('register', { title: 'Sign Up' });
+});
+
+
 // GET /about
 router.get('/about', function(req, res, next) {
   return res.render('about', { title: 'About' });
