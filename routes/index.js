0fdc1ff85d05b1570c972d5d5afa74a7ce99c24c
Adam Bodie <adam.bodie@yahoo.com>
2018-08-20 17:32:53 -0700

    Added mongoose dependency, User schema, updated register post route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const User = require('../models/user');
 
 // GET /
 router.get('/', function(req, res, next) {
@@ -13,7 +14,39 @@ router.get('/register', function(req, res, next) {
 
 // GET /register
 router.post('/register', function(req, res, next) {
-  return res.render('register', { title: 'Sign Up' });
+  if (req.body.email &&
+    req.body.name &&
+    req.body.dreamdestination &&
+    req.body.password &&
+    req.body.confirmPassword) {
+      // check to see if passwords match
+      if (req.body.password !== req.body.confirmPassword) {
+        const err = new Error('Passwords do not Match');
+        err.status = 400;
+        return next(err);
+      }
+
+      // create object with form input
+        const userData = {
+          email: req.body.email,
+          name: req.body.name,
+          dreamdestination: req.body.dreamdestination,
+          password: req.body.password,
+        }
+        // use schema's `create` method to insert data into mongo
+        User.create(userData, function(error, user) {
+          if (error) {
+            return next(error);
+          } else {
+            return res.redirect('/profile')
+          }
+        });
+
+    } else {
+      const err = new Error('All Fields Required');
+      err.status = 400;
+      return next(err);
+    }
 });
 
 
