30b7543ff802aeb4eed4ce6d7988657937ed442c
Adam Bodie <adam.bodie@yahoo.com>
2018-08-22 17:20:02 -0700

    Added bycrpt dependency

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const UserSchema = new mongoose.Schema({
     email:{
@@ -23,5 +24,17 @@ const UserSchema = new mongoose.Schema({
     }  
 });
 
+// hash password before saving
+UserSchema.pre('save', function(next){
+    var user = this;
+    bcrypt.hash(user.password, 10, function(err, hash){
+        if(err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+        
+    })
+})
 const User = mongoose.model('User', UserSchema);
 module.exports = User;
